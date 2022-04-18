f99e2cda07c5b3478a40b69198a160d7f24adca6
RAA80 <103951156+RAA80@users.noreply.github.com>
2022-04-18 19:36:54 +0300

    Add files via upload

---
+++ 360tv.lua
@@ -0,0 +1,42 @@
+-- script for 360tv.ru (10/04/2022)
+-- example: https://360tv.ru/air/live/
+-- example: https://360tv.ru/air/news/
+
+
+if m_simpleTV.Control.ChangeAddress ~= 'No' then return end
+
+local inAdr = m_simpleTV.Control.CurrentAddress
+if inAdr == nil then return end
+
+if not string.match(inAdr, '360tv%.ru/(.+)') then return end
+
+m_simpleTV.Control.ChangeAddress = 'Yes'
+m_simpleTV.Control.CurrentAddress = ''
+
+local proxy = ''    -- прокси: 'http://proxy-nossl.antizapret.prostovpn.org:29976'
+local session = m_simpleTV.Http.New('Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML,like Gecko) Chrome/79.0.2785.143 Safari/537.36', proxy, false)
+if session == nil then return end
+
+m_simpleTV.Http.SetTimeout(session, 10000)
+
+---------------------------------------------------------------------------
+
+local function _send_request(session, address)
+    local err, answer = m_simpleTV.Http.Request(session, {url=address})
+    if err ~= 200 then
+        m_simpleTV.Http.Close(session)
+        m_simpleTV.OSD.ShowMessage("Connection error: " .. err, 255, 3)
+        return
+    end
+
+    return answer
+end
+
+local answer = _send_request(session, inAdr)
+local url = string.match(answer, "src='(.-)'")
+
+local answer = _send_request(session, url)
+local url = string.match(answer, 'var src = "(.-)";')
+
+m_simpleTV.Http.Close(session)
+m_simpleTV.Control.CurrentAddress = url
