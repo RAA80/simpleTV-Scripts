85bd6cbe1707253b4ae036c6a7f729aa89da580b
RAA80 <aryadno@mail.ru>
2022-09-10 16:46:16 +0300

    update

--- scripts/matchtv.lua
+++ scripts/matchtv.lua
@@ -1,4 +1,4 @@
--- script for matchtv.ru (06/09/2022)
+-- script for matchtv.ru (10/09/2022)
 -- https://github.com/RAA80/simpleTV-Scripts
 
 -- example: https://matchtv.ru/on-air
@@ -22,8 +22,8 @@ m_simpleTV.Http.SetTimeout(session, 10000)
 
 ---------------------------------------------------------------------------
 
-local function _send_request(session, address, header)
-    local err, answer = m_simpleTV.Http.Request(session, {url=address, headers=header})
+local function _send_request(session, address)
+    local err, answer = m_simpleTV.Http.Request(session, {url=address})
     if err ~= 200 then
         m_simpleTV.Http.Close(session)
         m_simpleTV.OSD.ShowMessage("Connection error: " .. err, 255, 3)
@@ -33,11 +33,17 @@ local function _send_request(session, address, header)
     return answer
 end
 
-local answer = _send_request(session, inAdr, nil)
-local url = string.match(answer, 'data%-video%-player%-events%-target="iframe".-src="(.-)"')
+local answer = _send_request(session, inAdr)
+local url = string.match(answer, '<div class="video%-player.-src="([^"]+)')
 
-local answer = _send_request(session, url, 'Referer: ' .. url)
-local url = string.match(answer, "src: '(.-)'")
+local answer = _send_request(session, url)
+local url = string.match(answer, '"config%=(.-)"')
+
+local answer = _send_request(session, url)
+local url = string.match(answer, '<video_hd><!%[CDATA%[(.-)%]%]')
+
+local answer = _send_request(session, url)
+local url = string.match(answer, '<track .-><!%[CDATA%[(.-)%]%]></track>')
 
 m_simpleTV.Http.Close(session)
 m_simpleTV.Control.CurrentAddress = url
