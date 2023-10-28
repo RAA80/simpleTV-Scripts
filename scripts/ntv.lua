c676c4edf696a3eb56339f45396e1a37bed6c3c5
Ryadno Alexey <aryadno@mail.ru>
2023-10-28 11:52:48 +0300

    update

--- scripts/ntv.lua
+++ scripts/ntv.lua
@@ -1,8 +1,7 @@
--- script for ntv.ru (26/07/2023)
+-- script for ntv.ru (28/10/2023)
 -- https://github.com/RAA80/simpleTV-Scripts
 
--- example: https://www.ntv.ru/air/
--- example: https://www.ntv.ru/air/ntvhit/
+-- example: https://www.ntv.ru/air/ntv/
 -- example: https://www.ntv.ru/air/ntvlaw/
 -- example: https://www.ntv.ru/air/ntvseries/
 -- example: https://www.ntv.ru/air/ntvstyle/
@@ -27,27 +26,29 @@ m_simpleTV.Http.SetTimeout(session, 10000)
 
 ---------------------------------------------------------------------------
 
-local err, answer = m_simpleTV.Http.Request(session, {url='https://www.ntv.ru/air'})
+local json = require "rxijson"
+
+local err, answer = m_simpleTV.Http.Request(session, {url='https://www.ntv.ru/api/new/widgets/air/source/index.jsp'})
 if err ~= 200 then
     m_simpleTV.Http.Close(session)
     m_simpleTV.OSD.ShowMessage("Connection error: " .. err, 255, 3)
     return
 end
 
+local tab = json.decode(answer)
 local url
-if string.match(inAdr, '/air/$') then
-    url = string.match(answer, "var hdHlsURL = '(.-)'")
-elseif string.match(inAdr, '/air/unknown_russia/') then
-    url = string.match(answer, "var unknownRussiaHlsURL = '(.-)'")
-elseif string.match(inAdr, '/air/ntvseries/') then
-    url = string.match(answer, "var serialHlsURL = '(.-)'")
-elseif string.match(inAdr, '/air/ntvstyle/') then
-    url = string.match(answer, "var styleHlsURL = '(.-)'")
-elseif string.match(inAdr, '/air/ntvlaw/') then
-    url = string.match(answer, "var pravoHlsURL = '(.-)'")
-elseif string.match(inAdr, '/air/ntvhit/') then
-    url = string.match(answer, "var hitHlsURL = '(.-)'")
+
+if string.match(inAdr, '/ntvstyle/') then
+    url = tab.data.channels[1].src
+elseif string.match(inAdr, '/ntvseries/') then
+    url = tab.data.channels[2].src
+elseif string.match(inAdr, '/ntvlaw/') then
+    url = tab.data.channels[3].src
+elseif string.match(inAdr, '/ntv/') then
+    url = tab.data.channels[4].src
+elseif string.match(inAdr, '/unknown_russia/') then
+    url = tab.data.channels[5].src
 end
 
 m_simpleTV.Http.Close(session)
-m_simpleTV.Control.CurrentAddress = 'https:' .. url
+m_simpleTV.Control.CurrentAddress = url
